1153e635eba3da1c6b29b4e7b5682d77488b6215
MrUncle <50101968+MrTamfanX@users.noreply.github.com>
2019-05-31 03:25:28 +0700

    Add files via upload

---
+++ MrTamfanXCyberTeam.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/bash
+#KumpulanScriptMrTamfanXCyberTeam
+#Ini Script Punya MrTamfanX Ya Kaga Usah Record!!!
+#By MrTamfanX 
+#Jangan DiRecord Tod!!!
+clear
+cd tampar
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo  $blue "<|===================================|>"
+echo  $blue "<|      Kumpulan Script MrTamfanX    |>"
+echo  $blue "<|                                   |>"
+echo  $blue "<| Name: MrTamfanX    ||    MrTamfanX|>"
+echo  $blue "<| MyTeam: MrTamfanX Cyber Team      |>"
+echo  $blue "<| Contact: +6285779515200           |>"
+echo  $blue "<| Thanks To: MrTamfanX Cyber Team   |>"
+echo  $blue "<|===================================|>"
+sleep 1
+echo  $green "============== $red MENU $green ================="
+echo  $blue "$red 1.Script Trojan MrTamfanXCyberTeam $blue"
+echo  $blue "$red 2.Script MCIDDOS MrTamfanXCyberTeam $blue"
+echo  $blue "$red 3.script MrRegisterTelepon $blue"
+echo  $blue "$red 4.script MrDownload Lagu $blue"
+echo  $blue "$red 5.Script TebasIndex MrTamfanXCyberTeam $blue"
+echo  $blue "$red 6.Script MrDeface MrTamfanXCyberTeam $blue"
+echo  $blue "$red 7.script MrCreateDeface $blue"
+echo  $blue "$red 8.Script MrTamfanSpam $blue"
+echo  $blue "$red 9.Script Tamfan Facebook $blue"
+echo  $blue "$red 10.Script TembakKuota $blue"
+echo  $blue "$red 11.Script PulsaGratis $blue"
+echo  $blue "$red 12.Script HackBank Fake $blue"
+echo  $blue "$red 13.informasi tools ini $blue"
+echo  $blue "$red 0.Selesai Mempakai ToolsIni $blue"
+echo  $cyan "========== $purple MasukanPilihan $cyan =========="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+
+clear
+if [ $MrTamfanX = 1 ] || [ $MrTamfanX = 01 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 3
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+sh Tamfan-Ddos.sh
+fi
+
+clear
+if [ $MrTamfanX = 2 ] || [ $MrTamfanX = 02 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 4
+git clone https://github.com/MrTamfanX/MCIDDOS
+cd MCIDDOS
+python2 TamfanDdos.py
+fi
+
+clear
+if [ $MrTamfanX = 3 ] || [ $MrTamfanX = 03 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 4
+git clone https://github.com/MrTamfanX/MrRegisterTelepon
+cd MrRegisterTelepon
+sh MrLagu.sh
+fi
+
+clear
+if [ $MrTamfanX = 4 ] || [ $MrTamfanX = 04 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 3
+git clone https://github.com/MrTamfanX/MrDownloadLagu
+cd MrDownloadLagu
+sh MrLagu.sh
+fi
+
+clear
+if [ $MrTamfanX = 5 ] || [ $MrTamfanX = 05 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 5
+git clone https://github.com/MrTamfanX/TebasIndex
+cd TebasIndex
+sh Tamfan.sh
+fi
+
+clear
+if [ $MrTamfanX = 6 ] || [ $MrTamfanX = 06 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 3
+git clone https://github.com/MrTamfanX/MrDeface
+cd MrDeface
+php MrDeface.php
+fi
+
+clear
+if [ $MrTamfanX = 7 ] || [ $MrTamfanX = 07 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 5
+git clone https://github.com/MrTamfanX/MrCreateScriptDeface
+cd MrCreateScriptDeface
+python2 MrCreateDeface.py
+fi
+
+clear
+if [ $MrTamfanX = 8 ] || [ $MrTamfanX = 08 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+sleep 6
+git clone https://github.com/MrTamfanX/MrTamfanSpam
+cd MrTamfanSpam
+sh MrTamfanSpam.sh
+fi
+
+clear
+if [ $MrTamfanX = 9 ] || [ $MrTamfanX = 09 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+git clone https://github.com/MrTamfanX/TamfanFacebook
+cd TamfanFacebook
+sh install.sh
+sh tamfan.sh
+fi
+
+clear
+if [ $MrTamfanX = 10 ] || [ $MrTamfanX = 010 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+git clone https://github.com/MrTamfanX/TembakKuota
+cd TembakKuota
+sh IsiKuota.sh
+fi
+
+clear
+if [ $MrTamfanX = 11 ] || [ $MrTamfanX = 011 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+git clone https://github.com/MrUncle/PulsaGratis
+cd PulsaGratis
+sh Pulsa.sh
+fi
+
+clear
+if [ $MrTamfanX = 12 ] || [ $MrTamfanX = 012 ]
+then
+figlet -f slant "MrTamfanXCyberTeam"|lolcat
+git clone https://github.com/MrUncle/Script-HackBank
+cd Script-HackBank
+sh ALLBANK
+fi
+
+clear
+if [ $MrTamfanX = 13 ] || [ $MrTamfanX = 013 ]
+then
+figlet -f slant "TerimaKasih"|lolcat
+sleep 3
+echo  $cyan "Pesan   : Janganlah Kamu Merecord Punya Saya Dan Mengaku Ngaku Sebagai Punya Anda Apakah Anda Kaga Bisa Bikin Sendiri !!!"
+sleep 1
+echo  $red  "Website : https://tutoriatermuxmrtamfanx.blogspot.com/2019/05/selamat-datang-kembali-di-website.html?m=1"
+sleep 0.1
+echo  $blue "Contact : mrtamfanxcyberteam@gmail.com"
+sleep 7
+fi
+
+clear
+if [ $MrTamfanX = 0 ] || [ $MrTamfanX = 00 ]
+then
+echo $red "Author  : AdminMrTamfanX"
+echo $blue "Contact : mrtamfanxcyberteam@gmail.com"
+echo $cyan "Website : https://tutoriatermuxmrtamfanx.blogspot.com/?m=1"
+echo $blue "Github  : https://github.com/MrTamfanX"
+echo $red "Pesan   : Kau Record Punya Saya MrTamfanX Cyber Team Pecah Pala Kau"
+echo $blue"╭──[Apakah Admin Tamfan]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 3
+echo $blue"[Terima Kasih Tod Silahkan Ngeue Dulu]"
+sleep 4
+echo $blue"╭──[Pencet Enter/Exit Untuk Keluar]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 6
+fi
+exit
+done
+
+
